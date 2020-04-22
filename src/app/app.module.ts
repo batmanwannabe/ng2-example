d355666b8ef73d9df50203ea96e1a620c7c0e710
solomon911 <solomonsahayaraj911@gmail.com>
2020-04-23 00:19:07 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, Ng2SmartTableModule ],
+  imports:      [ BrowserModule, FormsModule, Ng2SmartTableModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
